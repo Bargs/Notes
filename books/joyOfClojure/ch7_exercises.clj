69856e7a54a6eeec6592dc1cfd76d48bcee67f4d
Matt Bargar <mbargar@gmail.com>
2014-01-14 23:36:06 -0500

    Functions as arguments

--- books/joyOfClojure/ch7_exercises.clj
+++ books/joyOfClojure/ch7_exercises.clj
@@ -8,3 +8,16 @@
 ((fnth 5) '[a b c d e])
 
 ((partial + 5) 100 200)
+
+
+;; Creating a higher level function from existing parts by passing
+;; functions as argments
+
+(def plays [{:band "Burial", :plays 979, :loved 9}
+            {:band "Eno", :plays 2333, :loved 15}
+            {:band "Bill Evans", :plays 979, :loved 9}
+            {:band "Magma", :plays 2665, :loved 31}])
+
+(def sort-by-loved-ratio (partial sort-by #(/ (:plays %) (:loved %))))
+
+(sort-by-loved-ratio plays)
