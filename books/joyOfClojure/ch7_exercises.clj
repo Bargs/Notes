4a8fcf2815dcb5683d55556c7e179b831a85a119
Matt Bargar <mbargar@gmail.com>
2014-01-22 12:11:49 -0500

    Starting A* section

--- books/joyOfClojure/ch7_exercises.clj
+++ books/joyOfClojure/ch7_exercises.clj
@@ -130,3 +130,108 @@
           #(+ %1 %2)))
 
 (tri 10)
+
+
+;; A* pathfinding
+
+;; Our 2d world
+(def world [[  1   1   1   1   1]
+            [999 999 999 999   1]
+            [  1   1   1   1   1]
+            [  1 999 999 999 999]
+            [  1   1   1   1   1]])
+
+
+;; Neighbors function from chapter 5.
+;; Takes a grid size and coordinates. Calculates
+;; the coordinates of positions N, S, E, and W of
+;; the current position. Filters out all invalid
+;; coordinates (< 0 or >= size). Returns a vector
+;; of coordinates for all valid neighbors.
+(defn neighbors
+  ([size yx]
+   (neighbors [[-1 0] [1 0] [0 -1] [0 1]]
+              size
+              yx))
+  ([deltas size yx]
+   (filter (fn [new-yx]
+             (every? #(< -1 % size) new-yx))
+           (map #(vec (map + yx %))
+                deltas))))
+
+(neighbors 5 [0 0])
+
+
+;; Our estimated cost function
+(defn estimate-cost [step-cost-est size y x]
+  (* step-cost-est
+     (- (+ size size) y x 2)))
+
+(estimate-cost 900 5 0 0)
+
+(estimate-cost 900 5 4 4)
+
+
+;; Our "cost so far" function
+(defn path-cost [node-cost cheapest-nbr]
+  (+ node-cost
+     (or (:cost cheapest-nbr) 0)))
+
+(path-cost 900 {:cost 1 })
+
+
+;; Our total cost function
+(defn total-cost [newcost step-cost-est size y x]
+  (+ newcost
+     (estimate-cost step-cost-est size y x)))
+
+(total-cost 0 900 5 0 0)
+
+(total-cost 1000 900 5 3 4)
+
+(total-cost (path-cost 900 {:cost 1}) 900 5 3 4)
+
+
+;; A helper function for determining the minimum value
+;; in a collection based on some function
+(defn min-by [f coll]
+  (when (seq coll)
+    (reduce (fn [min other]
+              (if (> (f min) (f other))
+                other
+                min))
+            coll)))
+
+(min-by :cost [{:cost 100} {:cost 36} {:cost 9}])
+
+
+;; The main A* algorithm
+(defn astar [start-yx step-est cell-costs]
+  (let [size (count cell-costs)]
+    (loop [steps 0
+           routes (vec (replicate size (vec (replicate size nil))))
+           work-todo (sorted-set [0 start-yx])]
+      (if (empty? work-todo)
+        [(peek (peek routes)) :steps steps]
+        (let [[_ yx :as work-item] (first work-todo)
+              rest-work-todo (disj work-todo work-item)
+              nbr-yxs (neighbors size yx)
+              cheapest-nbr (min-by :cost
+                                   (keep #(get-in routes %)
+                                         nbr-yxs))
+              newcost (path-cost (get-in cell-costs yx)
+                                 cheapest-nbr)
+              oldcost (:cost (get-in routes yx))]
+          (if (and oldcost (>= newcost oldcost))
+            (recur (inc steps) routes rest-work-todo)
+            (recur (inc steps)
+                   (assoc-in routes yx
+                             {:cost newcost
+                              :yxs (conj (:yxs cheapest-nbr [])
+                                         yx)})
+                   (into rest-work-todo
+                         (map
+                          (fn [w]
+                            (let [[y x] w]
+                              [(total-cost newcost step-est size y x) w]))
+                          nbr-yxs)))))))))
