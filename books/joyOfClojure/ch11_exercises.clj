e8a7d54b7aba238fe440c0e756806ae90c9bdae3
mbargar <mbargar@gmail.com>
2014-03-24 20:55:50 -0400

    starting futures example

--- books/joyOfClojure/ch11_exercises.clj
+++ books/joyOfClojure/ch11_exercises.clj
@@ -2,3 +2,15 @@
 
 (time (let [x (future (do (Thread/sleep 5000) (+ 41 1)))]
         [@x @x]))
+
+
+;; Creating a Twitter feed parser using futures
+
+(ns joy.futures
+  (:require [clojure [xml :as xml]]
+           [clojure [zip :as zip]])
+  (:import (java.util.regex Pattern)))
+
+(defn feed->zipper [uri-str]
+  (->> (xml/parse uri-str)
+       zip/xml-zip))
