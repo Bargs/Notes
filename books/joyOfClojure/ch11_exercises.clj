dcb410477a9adefff7506fbe493614568b36dcf6
Matt Bargar <mbargar@gmail.com>
2014-03-28 17:41:52 -0400

    finishing futures section

--- books/joyOfClojure/ch11_exercises.clj
+++ books/joyOfClojure/ch11_exercises.clj
@@ -69,3 +69,20 @@
   (reduce + (map deref results)))
 
 
+(defmacro as-futures [[a args] & body]
+  (let [parts (partition-by #{'=>} body)
+        [acts _ [res]] (partition-by #{:as} (first parts))
+        [_ _ task] parts]
+    `(let [~res (for [~a ~args] (future ~@acts))]
+       ~@task)))
+
+(defn occurrences [extractor tag & feeds]
+  (as-futures [feed feeds]
+              (count-text-task extractor tag feed)
+              :as results
+              =>
+              (reduce + (map deref results))))
+
+(occurrences title "released"
+             "http://blog.fogus.me/feed/"
+             "http://feeds.feedburner.com/ElixirLang")
