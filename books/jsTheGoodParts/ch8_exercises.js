66d2348eb0b77ceb03a1f64b001e4475282814d1
mbargar <mbargar@gmail.com>
2014-04-24 22:19:10 -0400

    function methods and some number methods

--- books/jsTheGoodParts/ch8_exercises.js
+++ books/jsTheGoodParts/ch8_exercises.js
@@ -95,3 +95,34 @@ a
 var a = ['a', 'b', 'c'];
 a.unshift('?', '@');
 a
+
+
+
+// function
+
+// function.apply(thisarg, argArray)
+// Invokes a function with and object to be bound to `this` and an optional array of args
+
+
+// Number
+
+// number.toExponential(fractionDigits)
+// Converts a number to a string in exponential form. `fractionDigits` controls the number of
+// decimal plcaes
+var tenThousand = 10000;
+tenThousand.toExponential(2);
+Math.PI.toExponential(0);
+Math.PI.toExponential(2);
+Math.PI.toExponential(7);
+Math.PI.toExponential();
+
+
+// number.toFixed(fractionDigits)
+// converts a number to a string in decimal form. `fractionDigits` is the number of decimals places
+// default is 0, should be between 0 and 20
+
+Math.PI.toFixed(0);
+Math.PI.toFixed(2);
+Math.PI.toFixed(7);
+Math.PI.toFixed(16);
+Math.PI.toFixed();
