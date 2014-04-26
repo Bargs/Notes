469538a8bf8cea8f482d4bcd8494ac838328d73a
mbargar <mbargar@gmail.com>
2014-04-25 20:12:57 -0400

    finished number, object, and regexp methods

--- books/jsTheGoodParts/ch8_exercises.js
+++ books/jsTheGoodParts/ch8_exercises.js
@@ -1,5 +1,31 @@
 // Ch 8 Methods
 
+
+// Some helpers for later on
+
+// A helper function for creating objects with a given prototype
+if (typeof Object.create !== 'function') {
+  Object.create = function (o) {
+    var F = function () {};
+    F.prototype = o;
+    return new F();
+  };
+}
+
+// replaces html special chars with their html entities
+if (!String.prototype.entityify) {
+    String.prototype.entityify = function () {
+        return this.replace(/&/g, "&amp;").replace(/</g,
+            "&lt;").replace(/>/g, "&gt;");
+    };
+}
+
+// Clears all html from the browser
+var clearDoc = function () {
+  document.body.innerHTML = "";
+}
+
+
 // Lots of examples of the standard methods on the standard types
 
 // Array
@@ -126,3 +152,67 @@ Math.PI.toFixed(2);
 Math.PI.toFixed(7);
 Math.PI.toFixed(16);
 Math.PI.toFixed();
+
+
+// number.toPrecision(precision)
+// Converts number to a string in decimal form with `precision` digits of precision.
+Math.PI.toPrecision(2);
+Math.PI.toPrecision(7);
+Math.PI.toPrecision(16);
+Math.PI.toPrecision();
+
+
+// number.toString(radix)
+// Converts number to a string. `radix` controls the base of the number, between 2 and 36.
+// Default is base 10.
+Math.PI.toString(2);
+Math.PI.toString(7);
+Math.PI.toString(16);
+Math.PI.toString();
+
+
+
+// Object methods
+
+
+// object.hasOwnProperty(name)
+// Returns true if the object contains a property having the name. Prototype chain is not checked.
+var a = {member: true};
+var b = Object.create(a);
+a.hasOwnProperty('member');
+b.hasOwnProperty('member');
+b.member
+
+
+
+// RegExp methods
+
+// regexp.exec(string)
+// Most powerful and slowest of the regexp methods. Returns an array, with 0 being the
+// matched text, 1 being capture group 1, 2 being group 2, etc. If the match fails null is returned.
+// If the regexp has a `g` flag, searching begins at `regexp.lastIndex` instead of position 0.
+// After a match `regexp.lastIndex` is set to the character after the match. An unsuccessful match
+// sets `regexp.lastIndex` back to 0. This allows you to search for multiple matches by executing
+// `regexp.exec(string)` in a loop.
+
+var text = '<html><body bgcolor=linen><p>' + 'This is <b>bold<\/b>!<\/p><\/body><\/html>';
+var tags = /[^<>]+|<(\/?)([A-Za-z]+)([^<>]*)>/g;
+var a, i;
+
+while ((a = tags.exec(text))) {
+  for (i = 0; i < a.length; i += 1) {
+    document.writeln(('// [' + i + '] ' + a[i]).entityify());
+    document.writeln('<br \/>');
+  }
+  document.writeln('<br \/><br \/>');
+}
+
+
+// regexp.test(string)
+// The simplest and fastest of the regexp methods. If there's a match, it returns true, otherwise
+// false. Don't use the `g` flag.
+
+var b = /&.+;/.test('frank &amp; beans');
+b
+
+
